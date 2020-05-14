ee81bc67c5a09a2f6bea5d9009ca7f6ef7bb82cd
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-05-13 21:39:02 -0500

    Changed a Message

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use('/api/users', userRouter)
 server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Welcome to Kara's 2nd Deployed Project API!</h2>`);
 });
 
 
